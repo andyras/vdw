df1f3a30c19541fa450ec8695e912ce54392f3c3
Andrew Rasmussen <andyras@gmail.com>
2013-07-10 15:47:22 -0500

    added function to sum voxel densities

--- build/src/voxel.cpp
+++ build/src/voxel.cpp
@@ -4,3 +4,16 @@
 bool compareVoxel(const voxel &a, const voxel &b) {
   return a.density < b.density;
 }
+
+/* Sum the densities in a vector of voxels */
+double sumVoxelDensity(const std::vector<voxel> * voxels) {
+  // accumulator
+  double summ = 0.0;
+
+  // sum the contents of the vector
+  for (int ii = voxels.begin(); ii < voxels.end(); ii++) {
+    summ += voxels[ii].density;
+  }
+
+  return summ;
+}
